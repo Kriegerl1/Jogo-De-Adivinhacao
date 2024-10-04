4e468e22e233e5d7044e04c78fe7e9cea6f315b7
Leonardo Rodrigues <himata666@hotmail.com>
2024-10-04 18:52:27 -0300

    Faltando ranking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,26 +11,24 @@ import { FormsModule } from "@angular/forms";
 })
 export class AppComponent {
   public title: string = "Jogo de Adivinhação";
+  public difficult: string = "";
+  public hiddenNumber: number = 0;
+  public chooseNumber: number = 0;
+  public lives: number = 0;
+  public score: number = 0;
+  public amountPossibility: number = 0;
+  public gameIsOver: boolean = false;
+  public triedNumbers: number[] = [];
+  public result: any;
+
   public progressBarColor: string = "";
+  public progressBarWidht: number = 0;
+  public percentOfProgressBar: number = 0;
+  public helperBar: number = 0;
   public hidder: string = "d-none";
   public message: string = "";
   public alert: string = "";
-  public difficult: string = "";
   public alertDifficult: string = "secondary";
-
-  public percentOfProgressBar: number = 0;
-  public amountPossibility: number = 50;
-  public progressBarWidht: number = 100;
-  public chooseNumber: number = 1;
-  public hiddenNumber: number = 0;
-  public helperBar: number = 0;
-  public score: number = 1000;
-  public lives: number = 0;
-
-  public triedNumbers: number[] = [];
-
-  public gameIsOver: boolean = false;
-  result: any;
 
   constructor() {
     this.replay();
@@ -39,39 +37,62 @@ export class AppComponent {
   public selectDifficult(difficult: string): void {
     this.difficult = difficult;
 
-    if (difficult == "Fácil") this.alertDifficult = "primary";
-    else if (difficult == "Médio") this.alertDifficult = "warning";
-    else this.alertDifficult = "danger";
+    switch (difficult) {
+      case "Fácil":
+        this.setupGame(10, "primary", 3);
+        break;
+      case "Médio":
+        this.setupGame(50, "warning", 5);
+        break;
+      case "Difícil":
+        this.setupGame(100, "danger", 7);
+        break;
+    }
+  }
+
+  private setupGame(
+    amountPossibility: number,
+    alertDifficult: string,
+    lives: number
+  ): void {
+    this.alertDifficult = alertDifficult;
+    this.amountPossibility = amountPossibility;
+    this.hiddenNumber = Math.floor(Math.random() * this.amountPossibility) + 1;
+    this.lives = lives;
   }
 
   public replay(): void {
     this.triedNumbers = [];
-    this.amountPossibility = 50;
-    this.score = 1000;
-
-    this.hiddenNumber = Math.floor(Math.random() * this.amountPossibility) + 1;
+    this.score = 100;
+    this.gameIsOver = false;
 
     this.progressBarWidht = 100;
     this.chooseNumber = 1;
     this.percentOfProgressBar = 0;
     this.helperBar = this.progressBarWidht;
-    this.gameIsOver = false;
 
     this.progressBarColor = "";
     this.hidder = "d-none";
     this.message = "";
     this.alert = "";
+
+    this.selectDifficult(this.difficult);
   }
 
   public calculateScore(chooseNumber: number): number {
-    this.result = this.hiddenNumber - chooseNumber;
+    const difference = Math.abs(this.hiddenNumber - chooseNumber);
 
-    return this.result;
+    if (difference >= 10) this.score -= 10;
+    else if (difference >= 5) this.score -= 4;
+    else this.score -= 2;
+
+    return this.score;
   }
 
   public guessNumber(): void {
-    if (this.chooseNumber > this.amountPossibility)
+    if (this.chooseNumber > this.amountPossibility) {
       this.chooseNumber = this.amountPossibility;
+    }
 
     this.percentOfProgressBar = Math.abs(
       (this.chooseNumber - this.hiddenNumber) *
@@ -79,30 +100,46 @@ export class AppComponent {
     );
 
     this.triedNumbers.push(this.chooseNumber);
-
     this.triedNumbers.sort((a, b) => a - b);
 
-    if (this.chooseNumber == this.hiddenNumber) {
-      this.message = "Voce Venceu!";
-      this.progressBarColor = "bg-success";
-      this.hidder = "";
-      this.alert = "success";
-
-      this.helperBar = this.progressBarWidht;
-      this.gameIsOver = true;
-    } else if (this.chooseNumber < this.hiddenNumber) {
-      this.message = "Tente um numero mais alto!";
-      this.progressBarColor = "bg-warning";
-      this.hidder = "";
-      this.alert = "warning";
-
-      this.helperBar = this.percentOfProgressBar;
+    if (this.lives >= 1) {
+      this.processGuess();
     } else {
-      this.message = "Tente um numero mais baixo!";
-      this.progressBarColor = "bg-warning";
-      this.hidder = "";
-      this.alert = "warning";
-      this.helperBar = this.percentOfProgressBar;
+      this.endGame("Você perdeu! :(", "bg-danger", "danger");
     }
+  }
+
+  private processGuess(): void {
+    if (this.chooseNumber == this.hiddenNumber) {
+      this.endGame("Você Venceu!", "bg-success", "success");
+    } else if (this.chooseNumber < this.hiddenNumber) {
+      this.handleIncorrectGuess("Tente um número mais alto!");
+      this.calculateScore(this.chooseNumber);
+    } else {
+      this.handleIncorrectGuess("Tente um número mais baixo!");
+      this.calculateScore(this.chooseNumber);
+    }
+  }
+
+  private handleIncorrectGuess(message: string): void {
+    this.lives -= 1;
+    this.message = message;
+    this.progressBarColor = "bg-warning";
+    this.hidder = "";
+    this.alert = "warning";
+    this.helperBar = this.percentOfProgressBar;
+  }
+
+  private endGame(
+    message: string,
+    progressBarColor: string,
+    alert: string
+  ): void {
+    this.message = message;
+    this.progressBarColor = progressBarColor;
+    this.hidder = "";
+    this.alert = alert;
+    this.helperBar = this.progressBarWidht;
+    this.gameIsOver = true;
   }
 }
